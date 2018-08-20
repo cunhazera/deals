d928b622f1d9e58a9cea9ffbb21f9e41a49c60ef
cunhazera <cunha.gabriel919@gmail.com>
2018-08-20 00:28:14 -0300

    changes

--- src/main/java/com/app/deals/repository/JDBCTemplates.java
+++ src/main/java/com/app/deals/repository/JDBCTemplates.java
@@ -16,11 +16,11 @@ public class JDBCTemplates {
     @Autowired
     private JdbcTemplate jdbc;
 
-    public static final String QUERY = "select du.stage_to as stage, count(*) as totalInStages from deals d inner join deals_updates du on d.deal_id = du.deal_id where year(du.update_date) = 2017 and week(du.update_date) between ? and ? and (select count(*) from deals_updates where year(update_date) = 2017 and deal_id = d.deal_id and week(deals_updates.update_date) = week(du.update_date)) = 1 group by du.stage_to";
+    public static final String QUERY = "select du.stage_to as stage, count(*) as totalInStages from deals d inner join deals_updates du on d.deal_id = du.deal_id where year(du.update_date) = 2017 and week(du.update_date) between ? and ? and (select count(*) from deals_updates where year(update_date) = 2017 and deal_id = d.deal_id and week(deals_updates.update_date) between ? and ?) = 1 group by du.stage_to";
 
     public List<DealVO> dealsNotMoved(int initial, int end) {
         List<DealVO> deals = jdbc.query(
-                QUERY, new BeanPropertyRowMapper<DealVO>(DealVO.class), initial, end
+                QUERY, new BeanPropertyRowMapper<DealVO>(DealVO.class), initial, end, initial, end
         );
         return deals;
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,6 +19,46 @@ $("#submit").click(function(e) {
         url = '/api/deals/unmoved?initial=' + initialWeek + '&end=' + endWeek + '&stage=' + stage;
     }
 
+    setInterval(function() {
+        request(url, negotiations);
+    }, 5000);
+
+});
+
+function processTotalUnmoved(items, level, element) {
+    clearHtml();
+    $(element).append('<ul class="list-group">');
+    for (var i = 0; i<items.length; i++ ) {
+        $(element).append('<li class="list-group-item d-flex justify-content-between align-items-center">'+items[i].stage+
+            '<span class="badge badge-primary badge-pill">'+items[i].totalInStages+'</span></li>');
+    }
+    $(element).append('</ul>');
+}
+
+function processDeals(items, level, element) {
+    clearHtml();
+
+    $(element).append('<thead> <tr> <th scope="col">Linha</th> <th scope="col">ID</th> <th scope="col">Negociação</th> <th scope="col">Status</th> <th scope="col">Usuário</th> </tr> </thead>');
+    $(element).append('<tbody>');
+    var markup = "<tr><th scope='row'>${nrow}</th><td>${id}</td><td>${id}</td><td>${status}</td><td>${user}</td></tr>";
+
+    for (var i = 0; i<items.length; i++ ) {
+        $.tmpl(markup, { "nrow" : i +1, "id":items[i].id, "deal":items[i].dealName, "status":items[i].status, "user":items[i].userName  }).appendTo(element);
+    }
+    $(element).append('</tbody>');
+    
+}
+
+function clearHtml() {
+    if ($('#deals').text().trim() != '') {
+        document.getElementById('deals').innerHTML = '';
+    }
+    if ($('#list').text().trim() != '') {
+        document.getElementById('list').innerHTML = '';
+    }
+}
+
+function request(url, negotiations) {
     $.ajax({
         type: "GET",
         url: url,
@@ -36,33 +76,4 @@ $("#submit").click(function(e) {
             console.log("naaaaaaaao");
         }
     });
-});
-
-function processTotalUnmoved(items, level, element) {
-    if ($('#list').text().trim() != '') {
-        document.getElementById('list').innerHTML = '';
-    }
-    $(element).append('<ul class="list-group">');
-    for (var i = 0; i<items.length; i++ ) {
-        $(element).append('<li class="list-group-item d-flex justify-content-between align-items-center">'+items[i].stage+
-            '<span class="badge badge-primary badge-pill">'+items[i].totalInStages+'</span></li>');
-    }
-    $(element).append('</ul>');
 }
-
-function processDeals(items, level, element) {
-    if ($('#deals').text().trim() != '') {
-        document.getElementById('deals').innerHTML = '';
-    }
-
-    $(element).append('<thead> <tr> <th scope="col">Linha</th> <th scope="col">ID</th> <th scope="col">Negociação</th> <th scope="col">Status</th> <th scope="col">Usuário</th> </tr> </thead>');
-    $(element).append('<tbody>');
-    var markup = "<tr><th scope='row'>${nrow}</th><td>${id}</td><td>${id}</td><td>${status}</td><td>${user}</td></tr>";
-
-    for (var i = 0; i<items.length; i++ ) {
-        $.tmpl(markup, { "nrow" : i +1, "id":items[i].id, "deal":items[i].dealName, "status":items[i].status, "user":items[i].userName  }).appendTo(element);
-    }
-    $(element).append('</tbody>');
-    
-}
-
